80802dde110c852caf62a066e6b23ef5ea81e347
Spr1tek1d <abhi130501@gmail.com>
2021-10-24 23:38:52 +0530

    ajax

---
+++ php/reject_action.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL ^ E_NOTICE);
+  include_once 'connect.php';
+    // Checking, if post value is
+    // set by user or not
+    if(isset($_POST['btnValue']))
+    {
+        // Getting the value of button
+        // in $btnValue variable
+        $btnValue = $_POST['btnValue'];
+        $btnValue = (integer)$btnValue;
+    $stmt = $conn->prepare("update request set Status='reject' where Leave_ID=?");
+    $stmt->bind_param("i", $btnValue);
+$stmt->execute();
+    }
+?>
